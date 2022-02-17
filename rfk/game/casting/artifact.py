b90151af2b1947e2d88135b827b50967d13c9da1
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-02-17 08:27:17 -0700

    RFK MR V1

---
+++ rfk/game/casting/artifact.py
@@ -0,0 +1,13 @@
+from game.casting.actor import Actor
+
+# TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
+class Artifact(Actor):
+
+    def __init__(self):
+        self._message = ""
+
+    def set_message(self, message):
+        self._message = message;
+
+    def get_message(self):
+        return(self._message)
